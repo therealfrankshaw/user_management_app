8d8b80d497d47723261e0710a4dbd3a5235e747b
Frank Shaw <fhshaw7@gmail.com>
2024-07-25 17:33:32 -0700

    add ability to create users
    AbstractUser expects a username, I don't need it here, so im setting the email as the username.
    currently anyone can create any user/user level. Will eventually need to revisit

--- frontend/src/AddTeamMember.jsx
+++ frontend/src/AddTeamMember.jsx
@@ -1,24 +1,76 @@
 import { useState, useEffect } from 'react'
 
 function AddTeamMember() {
+  const [formData, setFormData] = useState({
+    first_name: '',
+    last_name: '',
+    email: '',
+    number: '',
+    role: 'regular',
+  })
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormData({ 
+      ...formData, 
+      [name]: value
+    })
+  }
+
+  const addTeamMember = (e) => {
+    e.preventDefault()
+    fetch('http://127.0.0.1:8000/api/users/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+    .then((response) => {
+      if (!response.ok) {
+        console.log('User not created')
+        return response.json().then(err => Promise.reject(err));
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log('User created successfully:', data);
+      setFormData({
+        first_name: '',
+        last_name: '',
+        email: '',
+        number: '',
+        role: 'regular',
+      })
+    }).catch((error) => {
+      console.log('Error creating user:', error);
+      alert('Error creating user');
+    });
+  }
+
+  const formStyle = {
+    display: 'flex', 
+    flexDirection: 'column', 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  }
 
   return (
     <>
       <h1>Add a team member</h1>
       <p>set email, location, and role</p>
       <hr />
-      <form>
+      <form style={formStyle} onSubmit={addTeamMember}>
         <h2>info</h2>
-        <input type="text" placeholder="first name" />
-        <input type="text" placeholder="last_name" />
-        <input type="text" placeholder="email" />
-        <input type="text" placeholder="phone" />
+        <input type="text" placeholder="first name" name="first_name" value={formData.first_name} onChange={handleChange} />
+        <input type="text" placeholder="last_name" name="last_name" value={formData.last_name} onChange={handleChange} />
+        <input type="text" placeholder="email" name="email" value={formData.email} onChange={handleChange} />
+        <input type="text" placeholder="phone" name="number" value={formData.number} onChange={handleChange} />
+        <h2>Role</h2>
+        <label><input type="radio" name="role" value="regular" onChange={handleChange} checked={formData.role === 'regular'} /> Regular - Can't delete members</label>
+        <label><input type="radio" name="role" value="admin" onChange={handleChange} checked={formData.role === 'admin'} /> Admin - Can delete members</label>
+        <button>Add</button>
       </form>
-      <h2>Role</h2>
-      <label><input type="radio" name="role" value="regular" /> Regular - Can't delete members</label>
-      <label><input type="radio" name="role" value="admin" /> Admin - Can delete members</label>
-
-      <button>Add</button>
     </>
   )
 }
--- simple_team_member_management/members/serializers.py
+++ simple_team_member_management/members/serializers.py
@@ -5,3 +5,7 @@ class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id', 'first_name', 'last_name', 'number', 'email', 'role']
+
+    def create(self, validated_data):
+        validated_data['username'] = validated_data['email']
+        return super().create(validated_data)
