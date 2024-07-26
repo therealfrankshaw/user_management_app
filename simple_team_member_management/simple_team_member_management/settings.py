4bdb07c0a4e3d8bb88bc2135e5a4f2f6d991712f
Frank Shaw <fhshaw7@gmail.com>
2024-07-25 19:11:50 -0700

    oops, used during debug.

--- simple_team_member_management/simple_team_member_management/settings.py
+++ simple_team_member_management/simple_team_member_management/settings.py
@@ -132,5 +132,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CORS_ALLOW_ALL_ORIGINS = True
